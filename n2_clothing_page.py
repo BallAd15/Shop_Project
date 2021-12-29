bde7bae2d690879e80a6069b05f35492ef5d6832
Aditya Baliga <61866962+BallAd15@users.noreply.github.com>
2021-12-29 19:29:27 +0530

    Delete n2_clothing_page.py

--- n2_clothing_page.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-def clothing_page_funct(topframe,main_page):
-	for widget in topframe.winfo_children():
-		widget.destroy()
-	button1=Button(topframe, text = "↩ Return", fg = "white", bg = "black", command=main_page)
-	#button1.pack()
-	button1.config(width=15, font="Palatino")
-	button1.grid(row=1,column=1)
